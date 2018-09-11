a77dc893cd23243b8e5d4b53ef82e3643c7a3064
Cyberlacs <cyberlacs@gmail.com>
2018-09-11 09:07:31 -0300

    11-09-2018

---
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,39 @@
+﻿namespace Sales.ViewModels
+{
+    using Common.Models;
+    using Services;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using Xamarin.Forms;
+
+    public class ProductsViewModel : BaseViewModel
+    {
+        private ApiServices apiServices;
+        private ObservableCollection<Product> products;
+
+        public ObservableCollection<Product> Products
+        {
+            get { return this.products; }
+            set { this.SetValue(ref this.products, value); }
+        }
+
+        public ProductsViewModel()
+        {
+            this.apiServices = new ApiServices();
+            this.LoadProducts();
+        }
+
+        private async void LoadProducts()
+        {
+            var response = await this.apiServices.GetList<Product>("http://www.sistemavoltesempre.com.br/salesapi", "/api", "/product");
+            if (response.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
+                return;
+            }
+
+            var list = (List<Product>)response.Result;
+            this.Products = new ObservableCollection<Product>(list);
+        }
+    }
+}
